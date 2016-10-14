ce587319e524516d111abd02f1c3b749674734b0
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-10-15 00:12:14 +0200

    Document go-up packages

--- elisp/go-up.el
+++ elisp/go-up.el
@@ -1,15 +1,63 @@
-;;; go-up.el --- Summary here
+;;; go-up.el --- Quickly go to a specific parent directory in eshell
 
-;;
-;; TODO: Add silent flag (global variable)
-;; TODO: Change 'ignore-case' to global variable
-;;
+;; Copyright (C) 2016 Peter W. V. Tran-Jørgensen
 
+;; Author: Peter W. V. Tran-Jørgensen <peter.w.v.jorgensen@gmail.com>
+;; Maintainer: Peter W. V. Tran-Jørgensen <peter.w.v.jorgensen@gmail.com>
+;; URL: https://github.com/peterwvj/go-up
+;; Created: 14th October 2016
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24"))
+;; Keywords: eshell
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Package for quickly navigating to a parent folder in eshell.
+
+;; Package for quickly navigating to a specific parent directory in
+;; eshell without having to repeatedly type 'cd ..'. This is achieved
+;; using the 'go-up' function, which can be bound to an alias such as
+;; 'up'. As an example, assume that the current working directory is:
+;;
+;; /home/user/first/second/third/fourth/fifth $
+;;
+;; Now, in order to quicky go to (say) the directory named 'first' one
+;; simply executes:
+;;
+;; /home/user/first/second/third/fourth/fifth $ up fir
+;; /home/user/first $
+;;
+;; This command searches the current path from right to left for a
+;; directory that matches the user's input ('fir' in this case). If a
+;; match is found then eshell changes to that directory, otherwise it
+;; does nothing.
+;;
+;; It is recommended to invoke 'go-up' using an alias as done in the
+;; example above. This can (for example) be done by adding the
+;; following to your .eshell.aliases file:
+;;
+;; alias up go-up $1
+;;
+;; This package is inspired by 'bd', which uses bash to implement
+;; similar functionality.
+;; See: https://github.com/vigneshwaranr/bd
 
 ;;; Code:
+
+;;
+;; TODO: Change 'ignore-case' to global variable
+;;
 
 (defun get-sep (os)
   "Utility function to get file separator, e.g. '/', for an operating system, OS.
