b3eb0213215dbf7ec5b43dab2469a5c3b90a86d5
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-10-30 12:30:13 +0100

    Update custom configuration

--- elisp/custom.el
+++ elisp/custom.el
@@ -4,9 +4,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-replace-threshold 50)
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (yasnippet xterm-color w3m undo-tree tangotango-theme symon smart-mode-line-powerline-theme rainbow-delimiters persistent-scratch pdf-tools ox-reveal org-gcal naquadah-theme mu4e-alert move-text monokai-theme molokai-theme markdown-mode magit lua-mode langtool hlinum helm-mu helm-descbinds github-theme focus eshell-z eshell-up elpy edit-server dired+ darkokai-theme cyberpunk-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-monokai color-theme-molokai calfw buffer-move browse-kill-ring+ auctex anzu aggressive-indent ace-window ace-jump-mode ac-math))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
