66b23072e5ef639f8df096839867a4c6ae708ade
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-12-05 21:15:09 +0100

    Clean-up anzu configuration

--- custom.el
+++ custom.el
@@ -20,6 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anzu-mode-line ((t (:foreground "yellow" :weight bold))))
  '(cursor ((t (:background "yellow"))))
  '(eshell-prompt ((t (:foreground "green yellow" :weight bold))))
  '(linum ((t (:inherit (shadow default) :foreground "gray40"))))
--- init.el
+++ init.el
@@ -184,16 +184,6 @@
 (use-package anzu)
 (global-anzu-mode +1)
 
-(set-face-attribute 'anzu-mode-line nil
-                    :foreground "yellow" :weight 'bold)
-
-(custom-set-variables
- '(anzu-mode-lighter "")
- '(anzu-deactivate-region t)
- '(anzu-search-threshold 1000)
- '(anzu-replace-threshold 50)
- '(anzu-replace-to-string-separator " => "))
-
 (define-key isearch-mode-map [remap isearch-query-replace]  #'anzu-isearch-query-replace)
 (define-key isearch-mode-map [remap isearch-query-replace-regexp] #'anzu-isearch-query-replace-regexp)
 
