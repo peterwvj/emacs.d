3c064c7936cb234001fc4f22c592ac0b69c59e3f
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-01-05 20:18:21 +0100

    Re-organise LaTeX config

--- elisp/latex-config-pvj.el
+++ elisp/latex-config-pvj.el
@@ -1,69 +1,70 @@
+;;
+;; LaTeX configuration
+;;
 
 (use-package tex
-  :ensure auctex)
+  :ensure auctex
+  :config
 
-;;
-;; Set AuxTex to PDF mode
-;;
-(setq TeX-PDF-mode t)
+  (setq TeX-PDF-mode t)
 
-;; Generate sync file and sync with C-v
-(eval-after-load
-    "tex" '(add-to-list 'TeX-command-list
-                        '("latexmk" "latexmk -pdf %t --synctex=1" TeX-run-TeX)))
+  ;; Generate sync file and sync with C-v
+  (eval-after-load
+      "tex" '(add-to-list 'TeX-command-list
+                          '("latexmk" "latexmk -pdf %t --synctex=1" TeX-run-TeX)))
 
-(setq latex-run-command "pdflatex")
-(setq LaTeX-command "latex --synctex=1")
+  (setq latex-run-command "pdflatex")
+  (setq LaTeX-command "latex --synctex=1")
 
-;; Use pdf-tools to open PDF files
-(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-      TeX-source-correlate-start-server t
-      TeX-source-correlate-method 'synctex)
-;;; AUCTeX config
-(setq TeX-auto-save t
-      TeX-parse-self t)
+  ;; Use pdf-tools to open PDF files
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+        TeX-source-correlate-start-server t
+        TeX-source-correlate-method 'synctex)
 
-;; Needed to sync TeX and PDF
-(add-hook 'LaTeX-mode-hook
-          '(lambda ()
-             (TeX-source-correlate-mode 1)))
+  (setq TeX-auto-save t
+        TeX-parse-self t)
 
-(add-hook 'TeX-after-compilation-finished-functions
-          #'TeX-revert-document-buffer)
+  ;; Needed to sync TeX and PDF
+  (add-hook 'LaTeX-mode-hook
+            '(lambda ()
+               (TeX-source-correlate-mode 1)))
 
-;; (add-hook 'pdf-view-mode-hook 'auto-revert-mode)
-;; (setq auto-revert-interval 0.5)
+  (add-hook 'TeX-after-compilation-finished-functions
+            #'TeX-revert-document-buffer)
 
-(add-hook 'pdf-view-mode-hook
-          (lambda ()
-            (linum-mode -1) ))
+  ;; (add-hook 'pdf-view-mode-hook 'auto-revert-mode)
+  ;; (setq auto-revert-interval 0.5)
 
-(add-hook 'pdf-view-mode-hook
-          (lambda ()
-            (pdf-view-fit-page-to-window) ))
+  (add-hook 'pdf-view-mode-hook
+            (lambda ()
+              (linum-mode -1) ))
 
-;; LateX keywords that need colouring
- (setq font-latex-match-reference-keywords
-  '(
-    ("ac" "[{")
-    ("todo" "[{")
-    ("kw" "[{")
-    ("vsl" "[{")
-    ("vpp" "[{")
-    ("vrt" "[{")
-    ("acp" "[{")
-    ("contribution" "[{")
-    ("acrodef" "[{")
-    ("crefName" "[{")
-    ("cref" "[{")
-    ("Cref" "[{")
-    ("cnref" "[{")
-    ("vdmkw" "[{")
-))
+  (add-hook 'pdf-view-mode-hook
+            (lambda ()
+              (pdf-view-fit-page-to-window) ))
 
-(add-hook 'LaTeX-mode-hook
-          '(lambda ()
-             (reftex-mode)))
+  ;; LateX keywords that need colouring
+  (setq font-latex-match-reference-keywords
+        '(
+          ("ac" "[{")
+          ("todo" "[{")
+          ("kw" "[{")
+          ("vsl" "[{")
+          ("vpp" "[{")
+          ("vrt" "[{")
+          ("acp" "[{")
+          ("contribution" "[{")
+          ("acrodef" "[{")
+          ("crefName" "[{")
+          ("cref" "[{")
+          ("Cref" "[{")
+          ("cnref" "[{")
+          ("vdmkw" "[{")
+          ))
+
+  (add-hook 'LaTeX-mode-hook
+            '(lambda ()
+               (reftex-mode))))
 
 ;;
 ;; Retrieve BibTeX entries
