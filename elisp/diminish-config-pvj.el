83cac7933f0e3883d152e243d87c25a9efa7395d
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-01-05 19:20:42 +0100

    Update diminish related configuration

--- elisp/diminish-config-pvj.el
+++ elisp/diminish-config-pvj.el
@@ -1,43 +1,47 @@
-(use-package diminish)
+;;
+;; diminish configuration
+;;
 
-(eval-after-load "undo-tree"
-  '(diminish 'undo-tree-mode))
+(use-package diminish
+  :config
+  (eval-after-load "undo-tree"
+    '(diminish 'undo-tree-mode))
 
-(eval-after-load "projectile"
-  '(diminish 'projectile-mode))
+  (eval-after-load "projectile"
+    '(diminish 'projectile-mode))
 
-(eval-after-load "helm-mode"
-  '(diminish 'helm-mode))
+  (eval-after-load "helm-mode"
+    '(diminish 'helm-mode))
 
-;; Use '✈' to show that flyspell is enabled
-(eval-after-load "flyspell"
-  '(diminish 'flyspell-mode (string 32 #x2708)))
+  ;; Use '✈' to show that flyspell is enabled
+  (eval-after-load "flyspell"
+    '(diminish 'flyspell-mode (string 32 #x2708)))
 
-(add-hook 'lisp-mode-hook
-          (lambda()
-            (setq mode-name "λ"))) 
+  (add-hook 'lisp-mode-hook
+            (lambda()
+              (setq mode-name "λ"))) 
 
-(add-hook 'emacs-lisp-mode-hook 
-          (lambda()
-            (setq mode-name "ξλ"))) 
+  (add-hook 'emacs-lisp-mode-hook 
+            (lambda()
+              (setq mode-name "ξλ"))) 
 
-(eval-after-load "reftex"
-  '(diminish 'reftex-mode))
+  (eval-after-load "reftex"
+    '(diminish 'reftex-mode))
 
-(eval-after-load "abbrev"
-  '(diminish 'abbrev-mode))
+  (eval-after-load "abbrev"
+    '(diminish 'abbrev-mode))
 
-(eval-after-load "hungry-delete"
-  '(diminish 'hungry-delete-mode))
+  (eval-after-load "hungry-delete"
+    '(diminish 'hungry-delete-mode))
 
-(eval-after-load "volatile-highlights"
-  '(diminish 'volatile-highlights-mode))
+  (eval-after-load "volatile-highlights"
+    '(diminish 'volatile-highlights-mode))
 
-(eval-after-load "google-this"
-  '(diminish 'google-this-mode))
+  (eval-after-load "google-this"
+    '(diminish 'google-this-mode))
 
-(diminish 'visual-line-mode)
+  (diminish 'visual-line-mode)
 
-(diminish 'outline-minor-mode)
+  (diminish 'outline-minor-mode))
 
 (provide 'diminish-config-pvj)
