bd73648e6d4ba41e212054c4b283b9fd3cc8ace2
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-01-08 14:19:29 +0100

    Fix mu4e-alert configuration

--- elisp/mu4e-config-pvj.el
+++ elisp/mu4e-config-pvj.el
@@ -211,11 +211,14 @@
                '(unread "Unread messages" ?u))
 
   ;; Email notifications
-  (mu4e-alert-set-default-style 'libnotify)
-  ;; (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-  (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
-  ;; (setq mu4e-alert-notify-repeated-mails t)
-  (setq mu4e-alert-interesting-mail-query unread)
+  (use-package mu4e-alert
+    :init
+    ;; (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+    (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
+    :config
+    (mu4e-alert-set-default-style 'libnotify)
+    ;; (setq mu4e-alert-notify-repeated-mails t)
+    (setq mu4e-alert-interesting-mail-query unread))
 
   ;; C-c C-a	` attach a file (pro-tip: drag & drop works as well)
 
