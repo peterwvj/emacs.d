c9be71e27c854d783c29ce2dfdb0042c26e45773
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-01-08 22:15:18 +0100

    Use lorem-ipsum

--- elisp/text-config-pvj.el
+++ elisp/text-config-pvj.el
@@ -98,17 +98,8 @@ there's a region, all lines that region covers will be duplicated."
       (message "Found duplicated word.")
     (message "End of buffer")))
 
-;; Inspired by https://github.com/jhaubrich/emacs
-(defun pvj/lorem-ipsum ()
-  "Insert a lorem ipsum."
-  (interactive)
-  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-          "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-          "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-          "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-          "culpa qui officia deserunt mollit anim id est laborum."))
+;; Insert dummy pseudo Latin text
+(use-package lorem-ipsum)
 
 ;; Can be used to overload C-w so if no region is selected then the
 ;; last word is deleted.
