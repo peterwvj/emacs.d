eecd0a449ff55a7105d511ee748ac80d0383728b
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-02-26 21:15:30 +0100

    Update flycheck to use Emacs' load path

--- elisp/flycheck-config-pvj.el
+++ elisp/flycheck-config-pvj.el
@@ -7,6 +7,7 @@
   :init
   (add-hook 'after-init-hook #'global-flycheck-mode)
   :config
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (setq flycheck-emacs-lisp-load-path 'inherit))
 
 (provide 'flycheck-config-pvj)
