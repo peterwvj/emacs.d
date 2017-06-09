515df8becbc436071be72b3a9e59955a6d4753dd
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-06-09 14:24:01 +0200

    Add window-resize and dumb-jump hydra

--- lisp/hydra-config-pvj.el
+++ lisp/hydra-config-pvj.el
@@ -3,12 +3,34 @@
 ;;
 
 (use-package hydra
+  :bind
+  (("C-c j" . hydra-dumb-jump/body))
   :config
   (defhydra hydra-zoom ()
     "zoom"
     ("i" text-scale-increase "in")
     ("o" text-scale-decrease "out")
     ("0" (text-scale-set 0) "reset")
-    ("q" nil "quit")))
+    ("q" nil "quit"))
+
+  (defhydra hydra-window-resize ()
+    "resize windows"
+    ("<down>" shrink-window "shrink vertically")
+    ("<up>" enlarge-window "enlarge vertically")
+    ("<left>" shrink-window-horizontally "shrink horizontally")
+    ("<right>" enlarge-window-horizontally "enlarge vertically")
+    ("j" ace-window "jump to window")
+    ("b" balance-windows "balance windows")
+    ("q" nil "quit"))
+
+  (defhydra hydra-dumb-jump ()
+    "dumb jump"
+    ("g" dumb-jump-go "go")
+    ("b" dumb-jump-back "back")
+    ("l" dumb-jump-quick-look "look")
+    ("e" dumb-jump-go-prefer-external "external")
+    ("w" dumb-jump-go-other-window "window")
+    ("q" nil "Quit")))
+
 
 (provide 'hydra-config-pvj)
