be8f710d23f624ad90b78832088fb3e93de7e50e
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-10-09 22:32:56 +0200

    Use winum

--- lisp/mode-line-config-pvj.el
+++ lisp/mode-line-config-pvj.el
@@ -9,4 +9,11 @@
   (spaceline-spacemacs-theme)
   (spaceline-helm-mode))
 
+(use-package winum
+  :config
+  (setq winum-auto-assign-0-to-minibuffer nil
+        winum-auto-setup-mode-line nil
+        winum-ignored-buffers '(" *which-key*"))
+  (winum-mode))
+
 (provide 'mode-line-config-pvj)
