bd9e4377a151f32b531908137fe076d4149b8094
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-10-19 07:35:05 +0200

    Improve show-paren-match face

--- custom.el
+++ custom.el
@@ -23,4 +23,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(eshell-prompt ((t (:foreground "green yellow" :weight bold))))
- '(nlinum-current-line ((t (:inherit linum :foreground "gold" :weight bold)))))
+ '(nlinum-current-line ((t (:inherit linum :foreground "gold" :weight bold))))
+ '(show-paren-match ((t (:background "'default" :foreground "#def" :weight extra-bold)))))
