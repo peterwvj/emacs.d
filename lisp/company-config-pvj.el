4964555f0a44b352081a2fc0198abb450e7a9c7f
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-11-03 14:43:52 +0100

    Remove yas company backend

--- lisp/company-config-pvj.el
+++ lisp/company-config-pvj.el
@@ -14,14 +14,6 @@
   ;; https://emacs.stackexchange.com/questions/10837/how-to-make-company-mode-be-case-sensitive-on-plain-text
   (setq company-dabbrev-downcase nil)
   
-  (defun company-mode/backend-with-yas (backend)
-    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-        backend
-      (append (if (consp backend) backend (list backend))
-              '(:with company-yasnippet))))
-
-  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
-  
   (add-hook 'after-init-hook 'global-company-mode)
   (global-set-key (kbd "M-C-/") 'company-complete))
 
