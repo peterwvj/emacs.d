3146e1fd692ede3316597c6fa361e1ad2c3e8690
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-01-23 18:30:06 +0100

    Open visited file in default external program

--- lisp/util-pvj.el
+++ lisp/util-pvj.el
@@ -41,4 +41,19 @@ Argument REGION the paragraph region."
     (switch-to-buffer name)
     (other-window -1)))
 
+(defun pvj/open-with (arg)
+  "Open visited file in default external program.
+
+With a prefix ARG always prompt for command to use."
+  (interactive "P")
+  (when buffer-file-name
+    (shell-command (concat
+                    (cond
+                     ((and (not arg) (eq system-type 'darwin)) "open")
+                     ((and (not arg) (member system-type '(gnu gnu/linux gnu/kfreebsd))) "xdg-open")
+                     (t (read-shell-command "Open current file with: ")))
+                    " "
+                    (shell-quote-argument buffer-file-name)))))
+
+
 (provide 'util-pvj)
