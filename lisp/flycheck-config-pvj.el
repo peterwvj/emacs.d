2ca808231606f24073e3cc6d97ca6da0029c44c1
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-09-02 20:20:53 +0200

    Enable flycheck globally

--- lisp/flycheck-config-pvj.el
+++ lisp/flycheck-config-pvj.el
@@ -4,8 +4,8 @@
 
 ;; Syntax checking
 (use-package flycheck
-  ;; :init
-  ;; (add-hook 'after-init-hook #'global-flycheck-mode)
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode)
   :config
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
   (setq flycheck-emacs-lisp-load-path 'inherit))
