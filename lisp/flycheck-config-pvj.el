9128dd56d08173a48d1259b0ecdb81f33fd57a43
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-10-30 11:40:48 +0100

    Shows errors using pos-tip popups

--- lisp/flycheck-config-pvj.el
+++ lisp/flycheck-config-pvj.el
@@ -10,4 +10,10 @@
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
   (setq flycheck-emacs-lisp-load-path 'inherit))
 
+
+;; Shows errors using pos-tip popups
+(use-package flycheck-pos-tip
+  :config
+  (with-eval-after-load 'flycheck (flycheck-pos-tip-mode)))
+
 (provide 'flycheck-config-pvj)
