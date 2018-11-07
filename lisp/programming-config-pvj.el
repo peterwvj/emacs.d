096fd24d7272968fd7f1bd2dc50316944e685697
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-07 15:36:41 +0100

    Update comint config

--- lisp/programming-config-pvj.el
+++ lisp/programming-config-pvj.el
@@ -39,7 +39,6 @@ Argument STRING the compilation output."
 
 ;; comint configuration
 (setq comint-prompt-read-only t)
-(setq comint-scroll-to-bottom-on-input t)
 (setq comint-scroll-to-bottom-on-output t)
 (setq comint-move-point-for-output t)
 ;;
