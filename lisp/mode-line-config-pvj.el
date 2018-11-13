7fcfdbb2658519b7ac744be7fb48ca5c55abceb6
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-13 18:02:27 +0100

    Update mode-line config

--- lisp/mode-line-config-pvj.el
+++ lisp/mode-line-config-pvj.el
@@ -7,7 +7,12 @@
   :config
   (require 'spaceline-config)
   (spaceline-spacemacs-theme)
-  (spaceline-helm-mode))
+  (spaceline-helm-mode)
+  (spaceline-toggle-major-mode-off)
+  (spaceline-toggle-helm-help-off)
+  (spaceline-toggle-version-control-off)
+  (spaceline-toggle-buffer-size-off)
+  (spaceline-toggle-buffer-encoding-abbrev-off))
 
 (use-package winum
   :config
