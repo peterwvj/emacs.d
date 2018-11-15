e55e14e9a78c4cc0d110bb486dfab38f03b5bda4
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-15 20:49:12 +0100

    Update grep config

--- lisp/grep-config-pvj.el
+++ lisp/grep-config-pvj.el
@@ -2,14 +2,6 @@
 ;; Grep configuration
 ;;
 
-(defun rgrep-fullscreen () 
-  "Run `rgrep' in full screen."
-  (interactive)
-  (call-interactively 'rgrep)
-  (select-window (get-buffer-window "*grep*"))
-  (delete-other-windows))
-
-
 ;; Writable grep buffer
 (use-package wgrep
   :config
