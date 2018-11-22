b197f664199f7349e04455ef2103eeaaee958f2e
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-22 18:13:33 +0100

    Improve window navigation

--- lisp/window-config-pvj.el
+++ lisp/window-config-pvj.el
@@ -38,7 +38,19 @@
 	  (select-window first-win)
 	  (if this-win-2nd (other-window 1))))))
 
-
 (global-set-key (kbd "<f5>") 'pvj/toggle-window-split)
+
+(defun pvj/split-switch-window-vertically ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil))
+
+(defun pvj/split-switch-window-horizontally ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil))
+ 
+(global-set-key (kbd "C-x 2") 'pvj/split-switch-window-vertically)
+(global-set-key (kbd "C-x 3") 'pvj/split-switch-window-horizontally)
 
 (provide 'window-config-pvj)
