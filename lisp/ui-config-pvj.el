feffe06aab547dea60ec11b63a1eb4a5ae0e3dca
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-22 22:34:14 +0100

    Hide mode-line during start up to avoid UX glitches

--- lisp/ui-config-pvj.el
+++ lisp/ui-config-pvj.el
@@ -11,4 +11,33 @@
 (when (fboundp 'menu-bar-mode)
   (menu-bar-mode -1))
 
+;; Minor mode for hiding the mode-line
+;; from http://bzg.fr/emacs-hide-mode-line.html
+(defvar-local hidden-mode-line-mode nil)
+(defvar-local hide-mode-line nil)
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global t
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (force-mode-line-update)
+  ;; Apparently force-mode-line-update is not always enough to
+  ;; redisplay the mode-line
+  (redraw-display)
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden Mode Line Mode enabled.  "
+"Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
+;; Hide mode-line during start up to avoid UX glitches
+(hidden-mode-line-mode)
+
 (provide 'ui-config-pvj)
