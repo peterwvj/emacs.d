969bade448fe24c6b8b8678d70107532d563902a
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-23 21:54:11 +0100

    Update cursor config

--- custom.el
+++ custom.el
@@ -21,7 +21,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "LawnGreen"))))
  '(eshell-prompt ((t (:foreground "green yellow" :weight bold))))
  '(nlinum-current-line ((t (:inherit linum :foreground "gold" :weight bold))))
  '(region ((t (:background "MediumBlue" :foreground "White"))))
