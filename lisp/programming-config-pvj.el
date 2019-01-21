2327191e6ad11c5979576a98e6895a391387265f
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2019-01-21 14:54:33 +0100

    Add meghanada config

--- lisp/programming-config-pvj.el
+++ lisp/programming-config-pvj.el
@@ -176,4 +176,13 @@ Argument STRING the compilation output."
 (eval-after-load 'smartparens
   '(sp-local-pair #'vdm-comint-mode "`" nil :actions nil))
 
+(use-package meghanada
+  :config
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (setq c-basic-offset 4
+                    tab-width 4
+                    indent-tabs-mode t)
+              (meghanada-mode t))))
+
 (provide 'programming-config-pvj)
