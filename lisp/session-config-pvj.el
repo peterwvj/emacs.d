a54680cc3041a30fa40144144104010700d27fcb
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2019-04-10 18:05:00 +0200

    Add savehist-mode config

--- lisp/session-config-pvj.el
+++ lisp/session-config-pvj.el
@@ -2,6 +2,10 @@
 ;; Session configuration
 ;;
 
+(savehist-mode 1)
+
+(setq savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
+
 (setq history-length 1000)
 
 (provide 'session-config-pvj)
