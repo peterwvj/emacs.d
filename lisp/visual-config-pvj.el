85affb68de47c175e087c64deb11acd90aea741d
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2019-05-10 23:33:49 +0200

    Replace symbol-overlay with auto-highlight-symbol

--- lisp/visual-config-pvj.el
+++ lisp/visual-config-pvj.el
@@ -56,11 +56,25 @@
                             (setq-local global-hl-line-mode nil)))
 
 ;; highlight every occurence in buffer
-(use-package auto-highlight-symbol
-  :config
-  (add-hook 'prog-mode-hook 'auto-highlight-symbol-mode)
-  (setq ahs-idle-interval 1.0)
-  (setq ahs-default-range 'ahs-range-whole-buffer))
+(use-package symbol-overlay
+  :custom-face
+  (symbol-overlay-default-face ((t (:inherit 'region))))
+  (symbol-overlay-face-1 ((t (:inherit 'highlight))))
+  (symbol-overlay-face-2 ((t (:inherit 'font-lock-builtin-face :inverse-video t))))
+  (symbol-overlay-face-3 ((t (:inherit 'warning :inverse-video t))))
+  (symbol-overlay-face-4 ((t (:inherit 'font-lock-constant-face :inverse-video t))))
+  (symbol-overlay-face-5 ((t (:inherit 'error :inverse-video t))))
+  (symbol-overlay-face-6 ((t (:inherit 'dired-mark :inverse-video t :bold nil))))
+  (symbol-overlay-face-7 ((t (:inherit 'success :inverse-video t))))
+  (symbol-overlay-face-8 ((t (:inherit 'dired-symlink :inverse-video t :bold nil))))
+  :bind (("M-i" . symbol-overlay-put)
+         ("M-n" . symbol-overlay-jump-next)
+         ("M-p" . symbol-overlay-jump-prev)
+         ("M-N" . symbol-overlay-switch-forward)
+         ("M-P" . symbol-overlay-switch-backward)
+         ("M-C" . symbol-overlay-remove-all))
+  :hook ((prog-mode . symbol-overlay-mode)))
+
 
 (use-package highlight-numbers
   :init (add-hook 'prog-mode-hook 'highlight-numbers-mode))
