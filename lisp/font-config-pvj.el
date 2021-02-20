600b294ed6b37bb804b5660539730150057e3197
Peter W. V. Tran-Jørgensen <peter.w.v.jorgensen@gmail.com>
2021-02-20 10:07:37 +0100

    Adjust Source Code Pro font size

--- lisp/font-config-pvj.el
+++ lisp/font-config-pvj.el
@@ -7,7 +7,7 @@
     (cond ((member "Iosevka" fonts)
            (set-frame-font "Iosevka-14"))
           ((member "Source Code Pro" fonts)
-           (set-frame-font "Source Code Pro-14"))
+           (set-frame-font "Source Code Pro-12"))
           ((member "DejaVu Sans Mono" fonts)
            (set-frame-font "DejaVu Sans Mono-12")))))
 
